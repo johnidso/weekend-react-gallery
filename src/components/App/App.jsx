be2915a2f63111fdfea142b4d241fae6d172b0c1
John Idso <john.d.idso@gmail.com>
2021-07-09 20:35:52 -0500

    added file structure and began adding client side GET and PUT starting at app.jsx. currently untested

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [galleryList, setGalleryList] = useState([]);
+  
+  // GET photos from server
+  const getPhotos = () => {
+    Axios({
+      method: 'GET',
+      url:'/gallery'
+    })
+    .then( response => {
+      console.log('Getting Data:', response.data);
+      setGalleryList(response.data);
+    })
+    .catch( err => {
+      console.log('Error getting gallery', err);
+    })
+  }
+
+  // PUT photo likes
+  const photoLiked = (photoId) => {
+    axios({
+      method:'PUT',
+      url: `/gallery/like/${photoId}`
+    })
+    .then( response => {
+      getPhotos();
+    })
+    .catch( err => {
+      console.log('Error recording Like', err);
+    })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList />
       </div>
     );
 }
