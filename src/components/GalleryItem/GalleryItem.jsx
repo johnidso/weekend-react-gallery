a439f79193dd2c961b681a6d4403a42cec932753
John Idso <john.d.idso@gmail.com>
2021-07-10 15:38:46 -0500

    Base mode complete, conditional render of description added and tested.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,15 +1,27 @@
+import {useState} from 'react';
 import './GalleryItem.css';
 
 // set up gallery items, including photo, like button, and like(s) display
 function GalleryItem(props){
-    return( 
-    <section className="galleryItemWrapper">
-        <img src={props.path}/>
-        <div className='buttons'>
-            <button onClick = { () => props.photoLiked(props.id)}>Like</button>
-            <aside>Likes: {props.likes}</aside>
-        </div>
-    </section>
+    const [cardStatus, setCardStatus] = useState(true);
+    console.log(cardStatus);
+    return( (cardStatus) ? (
+        <section className="galleryItemWrapper">
+            <img src={props.path} onClick = { () => setCardStatus(!cardStatus)}/>
+            <div className='buttons'>
+                <button onClick = { () => props.photoLiked(props.id)}>Like</button>
+                <aside>Likes: {props.likes}</aside>
+            </div>
+        </section>
+        ) : (
+        <section className="galleryItemWrapper">
+            <p onClick = { () => setCardStatus(!cardStatus)}>{props.description}</p>
+            <div className='buttons'>
+                <button onClick = { () => props.photoLiked(props.id)}>Like</button>
+                <aside>Likes: {props.likes}</aside>
+            </div>
+        </section>
+        )
     )
 }
 
